c78c6a5be44ced44d4086eaa4520476bcd028c1f
Abhishekyadav3824 <aradaabhishek@gmail.com>
2025-09-13 09:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
+	event.preventDefault();
+	let firstName = document.forms["form1"]["fname"].value.trim();
+    let lastName = document.forms["form1"]["lname"].value.trim();
+	let fullName = firstName + " " + lastName;
+	alert(fullName);
     //Write your code here
 
 }
